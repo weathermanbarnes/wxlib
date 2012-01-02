fd65b0878743c2c2e6513f6bb5793e83edebff8d
Clemens Spensberger <csp001@uib.no>
2012-01-02 16:08:24 +0100

    Introduced constants module to contain version information; adapted compile script to use precompiler folder.

---
+++ dynlib_const.f95
@@ -0,0 +1,14 @@
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+! 		DynLib -- some constants
+! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!
+! Module maintained by Clemens Spensberger (csp001@uib.no)
+module config
+  use kind
+  !
+  character(len=48), parameter :: version  = 'dynlib ####VERREVI####'
+  character(len=48), parameter :: verdate  = '####VERDATE####'
+  character(len=48), parameter :: compiler = '####FCOMPIL####'
+  character(len=48), parameter :: comptime = '####FCOMTIM####'
+  character(len=48), parameter :: comphost = '####FCOMHOS####'
+end module
