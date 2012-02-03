b6fa380f10db472fd50c0ab7fc262d87c8979c27
Clemens Spensberger <csp001@uib.no>
2012-02-03 13:47:33 +0100

    Using the new binning subroutine in basic_stats.py .

--- basic_stats.py
+++ basic_stats.py
@@ -7,18 +7,18 @@ from metopen import metopen
 import static as c
 import dynlib
 
-q = 'defabs'
+q = 'defang'
 bench = True
 
-opath = '../deformation/'
+opath = '/Data/gfi/work/csp001/deformation'
 
 
 for plev in c.plevs:
 	nttot = 0
-	sum   = np.zeros((151,720))
-	sqsum = np.zeros((151,720))
+	sum   = np.zeros((361,720))
+	sqsum = np.zeros((361,720))
 
-	for year in c.years:
+	for year in range(1979,2000):
 		print 'Processing year %d, plev %d' % (year, plev)
 
 		f, dat   = metopen(c.file_std % (year, plev, q), c.q[q])
@@ -27,16 +27,28 @@ for plev in c.plevs:
 			begin = datetime.datetime.now()
 		#avg = dat.mean(axis=0)
 		#std = dat.std(axis=0)
-		avg, std = dynlib.stat.basic(dat)
+		min,max,avg,std = dynlib.stat.basic(dat)
+		mfv,his,med     = dynlib.stat.binned(dat, c.bins[q])
 		sum  [:,:] += nt*avg[:,:]
 		sqsum[:,:] += (nt-1)*std[:,:]**2+(2*nt-1)*avg[:,:]**2
+		if year == c.years[0]:
+			minv = min[:,:]
+			maxv = max[:,:]
+		else:
+			minv [:,:]  = np.minimum(min, minv)
+			maxv [:,:]  = np.maximum(max, maxv)
 		nttot      += nt
 		if bench:
 			print 'Fortran basic stats:', datetime.datetime.now()-begin
 		
 		ofile = opath+'/'+c.file_stat % (year, plev, q)
-		np.savez(ofile, mean=np.ascontiguousarray(avg.astype('f4')), 
-				stddev=np.ascontiguousarray(std.astype('f4')) )
+		np.savez(ofile, mean   = np.ascontiguousarray(avg.astype('f4')), 
+				stddev = np.ascontiguousarray(std.astype('f4')),
+				minv   = np.ascontiguousarray(min.astype('f4')),
+				maxv   = np.ascontiguousarray(max.astype('f4')), 
+				mfv    = np.ascontiguousarray(mfv.astype('f4')), 
+				hist   = np.ascontiguousarray(his.astype('f4')), 
+				median = np.ascontiguousarray(med.astype('f4'))  )
 		
 		if f:
 			f.close()
@@ -46,8 +58,10 @@ for plev in c.plevs:
 	sqsum[:,:] = np.sqrt((sqsum[:,:]-(2*nttot-1)*sum[:,:]**2)/(nttot-1))
 
 	ofile = opath+'/'+c.file_mstat % (plev, q)
-	np.savez(ofile, mean=np.ascontiguousarray(sum.astype('f4')), 
-			stddev=np.ascontiguousarray(sqsum.astype('f4')) )
+	np.savez(ofile, mean   = np.ascontiguousarray(sum.astype('f4')), 
+			stddev = np.ascontiguousarray(sqsum.astype('f4')),
+			minv   = np.ascontiguousarray(minv.astype('f4')),
+			maxv   = np.ascontiguousarray(maxv.astype('f4'))  ) 
 
 
 # the end
--- static.py
+++ static.py
@@ -2,19 +2,24 @@
 #  -*- encoding: utf-8
 
 import numpy as np
+import math
 
 plevs  = np.array([100,200,300,400,500,550,600,650,700,750,800,850,900,950,1000])
 years = range(1979,2009)
 
 q = {'defabs': 'defabs', 'defang': 'defang',
 	'u': 'u', 'v': 'v', 'Z': 'z' }
+_rose = [17,]
+_rose.extend(range(-18,18))
+bins = {'defabs': np.array(range(50))*2.0e-5,
+	'defang': np.array(_rose)*math.pi/36.0+math.pi/72.0 }
 
 datapath = ['/Data/gfi/work/csp001/deformation', '/scratch/reanalysis', '/media/work/reanalysis/highres', '/Data/gfi/share/Reanalyses/ERA_INTERIM/6HOURLY']
 file_std   = 'ei.ans.%d.%d.%s'
 file_stat  = 'ei.ans.%d.%d.%s.stat'
 file_mstat = 'ei.ans.stat.%d.%s'
 
-# Cut out    all times    lat > 15°N       all longitudes
-std_slice = (slice(None), slice(None,151), slice(None))
+# Cut out    all times   all latitudes all longitudes
+std_slice = (slice(None), slice(None), slice(None))
 
 #  the end
