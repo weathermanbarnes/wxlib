Added new default plot settings for some quantities, bugfix in masking in figures.py.

dynpie/settings.py
@@ -91,7 +91,15 @@ scale_defang_coarse = np.arange(-4,5)*np.pi/8.0 - np.pi/72.0
 ticks_defang = np.arange(-4,5)*3.1415926535/8.0 
 labls_defang = [u'-π/2', u'-3π/8', u'-π/4', u'-π/8', u'0', u'π/8', u'π/4', u'3π/8', u'π/2']
 
+scale_ow_mean = np.arange(-0.35, 0.36, 0.1)*1.0e-9
 
+scale_pvstir_mean = np.arange(-2.25, 2.26, 0.5)*1.0e-6
+scale_pvfil_mean  = np.arange(-7.0, 7.1, 2.0)*1.0e-6
+
+scale_rsr_mean = np.arange(-13.5, 13.6, 3.0)
+
+scale_q = np.arange(0.0, 10.1, 1.0)*1.0e-3
+scale_qfs = np.arange(-2.25,2.26,0.5)*1.0e-4
 
 # #############################################################################
 # 3. Default hooks for plotting
@@ -109,7 +117,7 @@ hooks['oro'] = _tmp
 # #############################################################################
 # 4. Default settings
 #
-Q = {'defabs': 'defabs', 'defang': 'defang', 'm': 'mont', 'p': 'pres', 'u': 'u', 'v': 'v', 'q': 'q', 'qstir': 'qfil',
+Q = {'defabs': 'defabs', 'defang': 'defang', 'm': 'mont', 'p': 'pres', 'u': 'u', 'v': 'v', 'q': 'q', 'qstir': 'qstir', 'qfil': 'qfil',
 		'T': 't', 'Z': 'z', 'oro': 'oro', 'rsr': 'rsr', 'ow': 'ow', 'pv': 'pv', 'pvstir': 'pvstir', 'pvfil': 'pvfil'}
 _rose = [17,]
 _rose.extend(range(-18,18))
@@ -156,7 +164,13 @@ DEFAULT_Q['Z']   = {'scale': scale_Z_diff}
 DEFAULT_Q['T']   = {'cmap': plt.cm.RdBu_r}
 DEFAULT_Q['pv']  = {'scale': scale_pv, 'hook': hooks['pv']}
 DEFAULT_Q['oro'] = {'scale': scale_oro_full, 'cmap': plt.cm.gist_earth, 'hook': hooks['oro']}
-
+DEFAULT_Q['ow']  = {'scale': scale_ow_mean, 'extend': 'both'}
+DEFAULT_Q['pvstir']  = {'scale': scale_pvstir_mean, 'extend': 'both'}
+DEFAULT_Q['pvfil']  = {'scale': scale_pvfil_mean, 'extend': 'both', 'cmap': plt.cm.PRGn}
+DEFAULT_Q['rsr']  = {'scale': scale_rsr_mean, 'extend': 'both'}
+DEFAULT_Q['q'] = {'scale': scale_q, 'extend': 'max', 'cmap': _get_q_cm()}
+DEFAULT_Q['qfil'] = {'scale': scale_qfs, 'extend': 'both', 'cmap': plt.cm.RdBu}
+DEFAULT_Q['qstir'] = {'scale': scale_qfs, 'extend': 'both'}
 
 
 # #############################################################################