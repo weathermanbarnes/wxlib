3357b61444adb2ea1765be013b65eeb6494fe6f9
Clemens Spensberger <clemens@spensberger.com>
2013-04-15 16:29:20 +1000

    Added new configuration key to hold the output path for plots (ppath), introduced autoscale module for more intelligent and configurable scaling.

--- dynpie/settings.py
+++ dynpie/settings.py
@@ -7,71 +7,14 @@ import math
 from collections import MutableMapping as mutmap
 
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-from proj import wmap
-
-
-
-# #############################################################################
-# 1. Colour maps
-# 
-def _get_greys_cm():
-	cdict = {'red':   ((0.0, 1.0, 1.0), (1.0, 0.1, 0.1)),
-		 'green': ((0.0, 1.0, 1.0), (1.0, 0.1, 0.1)),
-		 'blue':  ((0.0, 1.0, 1.0), (1.0, 0.1, 0.1))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_grey',cdict,256)
-
-def _get_defabs_cm():
-	cdict = {'red':   ((0.0, 1.0, 1.0), (0.33, 0.4, 0.4), (0.867, 1.0, 1.0), (1.0, 0.5, 0.5)),
-		 'green': ((0.0, 1.0, 1.0), (0.33, 0.5, 0.5), (0.867, 0.0, 0.0), (1.0, 0.2, 0.2)),
-		 'blue':  ((0.0, 1.0, 1.0), (0.33, 1.0, 1.0), (0.867, 0.0, 0.0), (1.0, 0.2, 0.2))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_defabs',cdict,256)
-
-def _get_defabs_cm2():
-	cdict = {'red':   ((0.0, 1.0, 1.0), (0.75, 0.15, 0.15), (1.0, 1.0, 1.0)),
-		 'green': ((0.0, 1.0, 1.0), (0.75, 0.15, 0.15), (1.0, 0.0, 0.0)),
-		 'blue':  ((0.0, 1.0, 1.0), (0.75, 0.15, 0.15), (1.0, 0.0, 0.0))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_defabs',cdict,256)
-
-def _get_q_cm():
-	cdict = {'red':   ((0.0, 1.0, 1.0), (0.33, 0.30, 0.30),  (0.867, 0.1, 0.1), (1.0, 0.5, 0.5)),
-		 'green': ((0.0, 1.0, 1.0), (0.33, 0.65, 0.65),  (0.867, 0.2, 0.2), (1.0, 0.2, 0.2)),
-		 'blue':  ((0.0, 1.0, 1.0), (0.33, 0.80, 0.80),  (0.867, 0.6, 0.6), (1.0, 0.8, 0.8))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_defabs',cdict,256)
-
-def _get_periodic_cm():
-	cdict = {'red':   ((0.0, 0.0, 0.0), (0.25, 0.8, 0.8), (0.5, 1.0, 1.0), (0.75, 0.0, 0.0), (1.0, 0.0, 0.0)),
-		 'green': ((0.0, 0.0, 0.0), (0.25, 0.0, 0.0), (0.5, 1.0, 1.0), (0.75, 0.9, 0.9), (1.0, 0.0, 0.0)),
-		 'blue':  ((0.0, 0.6, 0.6), (0.25, 0.0, 0.0), (0.5, 0.2, 0.2), (0.75, 0.0, 0.0), (1.0, 0.6, 0.6))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_periodic',cdict,256)
-
-def _get_periodic_cm2():
-	cdict = {'red':   ((0.0, 0.3, 0.3), (0.25, 0.4, 0.4), (0.5, 0.6, 0.6), (0.75, 0.8, 0.8), (1.0, 0.3, 0.3)),
-		 'green': ((0.0, 0.2, 0.2), (0.25, 0.8, 0.8), (0.5, 0.6, 0.6), (0.75, 0.4, 0.4), (1.0, 0.2, 0.2)),
-		 'blue':  ((0.0, 0.5, 0.5), (0.25, 0.0, 0.0), (0.5, 1.0, 1.0), (0.75, 0.0, 0.0), (1.0, 0.5, 0.5))  }
-
-	return mpl.colors.LinearSegmentedColormap('my_periodic2',cdict,256)
-
-
-def _get_periodic_cm3():
-	cdict = {'red':   ((0.0, 1.0, 1.0), (0.03, 0.9, 0.9), (0.27, 0.3, 0.2), (0.515, 0.5, 0.5), (0.76, 0.65, 0.7), (1.0, 1.0, 1.0)),
-		 'green': ((0.0, 1.0, 1.0), (0.03, 0.9, 0.9), (0.27, 0.3, 0.2), (0.515, 0.5, 0.5), (0.76, 0.65, 0.7), (1.0, 1.0, 1.0)),
-		 'blue':  ((0.0, 1.0, 1.0), (0.03, 1.0, 1.0), (0.27, 0.7, 0.7), (0.515, 0.5, 0.5), (0.76, 0.20, 0.3), (1.0, 0.9, 1.0)) }
-
-	return mpl.colors.LinearSegmentedColormap('my_periodic3',cdict,256)
-
-
+import proj
+import cm
 
 
 # #############################################################################
-# 2. Scales, ticks and labels
+# 1. Scales, ticks and labels
 # 
 scale_oro = range(10000,80001,10000)
 scale_oro_full = range(-19000,51000,2000)
@@ -89,7 +32,7 @@ scale_defabs_mean = np.arange(2.0,12.1,2.0)
 scale_defang = (np.arange(-18,19)-0.5)*np.pi/36.0
 scale_defang_coarse = np.arange(-4,5)*np.pi/8.0 - np.pi/72.0
 ticks_defang = np.arange(-4,5)*3.1415926535/8.0 
-labls_defang = [u'-π/2', u'-3π/8', u'-π/4', u'-π/8', u'0', u'π/8', u'π/4', u'3π/8', u'π/2']
+labels_defang = [u'-π/2', u'-3π/8', u'-π/4', u'-π/8', u'0', u'π/8', u'π/4', u'3π/8', u'π/2']
 
 scale_ow_mean = np.arange(-0.35, 0.36, 0.1)*1.0e-9
 
@@ -103,7 +46,7 @@ scale_qfs = np.arange(-1.65,1.66,0.3)*1.0e-4
 scale_qfs_mean = np.arange(-4.5,4.6,1.0)*1.0e-5
 
 # #############################################################################
-# 3. Default hooks for plotting
+# 2. Default hooks for plotting
 #
 hooks = {}
 hooks['defabs'] = lambda defabs: defabs*1e5
@@ -116,7 +59,7 @@ hooks['oro'] = _tmp
 
 
 # #############################################################################
-# 4. Default settings
+# 3. Default settings
 #
 Q = {'defabs': 'defabs', 'defang': 'defang', 'm': 'mont', 'p': 'pres', 'u': 'u', 'v': 'v', 'q': 'q', 'qstir': 'qstir', 'qfil': 'qfil',
 		'T': 't', 'Z': 'z', 'oro': 'oro', 'rsr': 'rsr', 'ow': 'ow', 'pv': 'pv', 'pvstir': 'pvstir', 'pvfil': 'pvfil', }
@@ -126,6 +69,7 @@ BINS_Q = {'defang': np.array(_rose)*math.pi/36.0+math.pi/72.0, }
 
 DATAPATH = ['.', '/Data/gfi/share/Reanalyses/ERA_INTERIM/6HOURLY']
 OPATH    = '.'
+PPATH    = '.'
 FILE_STD   = 'ei.ans.%d.%s.%s'
 FILE_STAT  = 'ei.ans.%d.%s.%s.stat'
 FILE_MSTAT = 'ei.ans.stat.%s.%s'
@@ -137,12 +81,12 @@ PVLEVS = ['pv2000', ]
 
 # DEFAULT contour settings
 if os.getenv('DYNLIB_PLOT_PRINT'):
-	DEFAULT_KWARGS = {'m': wmap, 'plev': '800', 'lon': None, 'lat': None, 'mark': None, 'scale': 10, 
+	DEFAULT_KWARGS = {'m': proj.wmap, 'plev': '800', 'lon': None, 'lat': None, 'mark': None, 'scale': 10, 
 		'overlays': [], 'disable_cb': True, 'show': False, 'save': '', 'title': '', 'hook': None,
 		'coastcolor': 'k', 'gridcolor': 'k', 'maskcolor': '0.25', 'orocolor': 'k', 'oroscale': scale_oro,
 		'oroalpha': 0.4, 'ticks': None, 'ticklabels': [] }
 else:
-	DEFAULT_KWARGS = {'m': wmap, 'plev': '800', 'lon': None, 'lat': None, 'mark': None, 'scale': 10, 
+	DEFAULT_KWARGS = {'m': proj.wmap, 'plev': '800', 'lon': None, 'lat': None, 'mark': None, 'scale': 10, 
 		'overlays': [], 'disable_cb': False, 'show': True, 'save': '', 'title': '', 'hook': None,
 		'coastcolor': 'k', 'gridcolor': 'k', 'maskcolor': '0.25', 'orocolor': 'k', 'oroscale': scale_oro,
 		'oroalpha': 0.4, 'ticks': None, 'ticklabels': [] }
@@ -157,9 +101,9 @@ MUTEX_GROUPS = [set(['colors', 'cmap']), ]
 
 # DEFAULT settings per quantity Q
 DEFAULT_Q = {}
-DEFAULT_Q['defabs'] = {'cmap': _get_defabs_cm2(), 'extend': 'max', 'scale': scale_defabs, 'hook': hooks['defabs']}
-DEFAULT_Q['defang'] = {'cmap': _get_periodic_cm3(), 'scale': scale_defang, 'ticks': ticks_defang, 
-	'ticklabels': labls_defang}
+DEFAULT_Q['defabs'] = {'cmap': cm.defabs2(), 'extend': 'max', 'scale': scale_defabs, 'hook': hooks['defabs']}
+DEFAULT_Q['defang'] = {'cmap': cm.periodic3(), 'scale': scale_defang, 'ticks': ticks_defang, 
+	'ticklabels': labels_defang}
 DEFAULT_Q['u']   = {'scale': scale_u_diff}
 DEFAULT_Q['Z']   = {'scale': scale_Z_diff}
 DEFAULT_Q['T']   = {'cmap': plt.cm.RdBu_r}
@@ -169,13 +113,13 @@ DEFAULT_Q['ow']  = {'scale': scale_ow_mean, 'extend': 'both'}
 DEFAULT_Q['pvstir']  = {'scale': scale_pvstir_mean, 'extend': 'both'}
 DEFAULT_Q['pvfil']  = {'scale': scale_pvfil_mean, 'extend': 'both', 'cmap': plt.cm.PRGn}
 DEFAULT_Q['rsr']  = {'scale': scale_rsr_mean, 'extend': 'both'}
-DEFAULT_Q['q'] = {'scale': scale_q, 'extend': 'max', 'cmap': _get_q_cm()}
+DEFAULT_Q['q'] = {'scale': scale_q, 'extend': 'max', 'cmap': cm.q()}
 DEFAULT_Q['qfil'] = {'scale': scale_qfs, 'extend': 'both', 'cmap': plt.cm.RdBu}
 DEFAULT_Q['qstir'] = {'scale': scale_qfs, 'extend': 'both', 'cmap': plt.cm.BrBG}
 
 
 # #############################################################################
-# 5. Making the settings easily available
+# 4. Making the settings easily available
 #
 class settings_dict(mutmap):
 	_mutexes = {}
@@ -370,6 +314,7 @@ class settings(object):
 		'bins': BINS_Q,
 		'datapath': DATAPATH,
 		'opath': OPATH,
+		'ppath': PPATH,
 		'file_std': FILE_STD,
 		'file_stat': FILE_STAT,
 		'file_mstat': FILE_MSTAT,
@@ -418,9 +363,9 @@ conf = settings()
 
 
 # #############################################################################
-# 6. Clean-Up: Making the default settings only available through settings objects
+# 5. Clean-Up: Making the default settings only available through settings objects
 # 
-del Q, BINS_Q, DATAPATH, OPATH, FILE_STD, FILE_STAT, FILE_MSTAT, STD_SLICE, YEARS, PLEVS, PTLEVS, PVLEVS
+del Q, BINS_Q, DATAPATH, OPATH, PPATH, FILE_STD, FILE_STAT, FILE_MSTAT, STD_SLICE, YEARS, PLEVS, PTLEVS, PVLEVS
 del DEFAULT_KWARGS, DEFAULT_CONTOUR_KWARGS, DEFAULT_CONTOURF_KWARGS, DEFAULT_Q, MUTEX_GROUPS
 
 
