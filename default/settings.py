3cd1ed0c891e8fc297506eea067627398480acc7
Clemens Spensberger <clemens@spensberger.com>
2013-04-21 10:00:14 +1000

    Added autoscale, moved colormaps to own file for easier import.

--- default/settings.py
+++ default/settings.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
-from dynlib.dynpie.settings import conf
-from dynlib.dynpie.proj import *
+from dynlib.dynpie.settings import conf as c
+import dynlib.dynpie.proj as proj
+import dynlib.dynpie.cm as cm
+
 
 # Adapt the default settings to your needs here, then use 
-# >>> from settings import conf
+# >>> from settings import *
 # in your personal scripts. 
 #
 # Examples:
 # (1) Add a folder to the datapath which is scanned in metopen. 
 #     The first argument is the location within the path, the second place in the example below.
-# >>> conf.datapath.insert(1, '/work/csp001/deformation')
+# >>> c.datapath.insert(1, '/work/csp001/deformation')
 # (2) Change the default colormap used in contourf-plots for the quantity 'Z'
-# >>> conf.contourf.Z['cmap'] = plt.cm.RdBu_r
+# >>> c.contourf.Z['cmap'] = plt.cm.RdBu_r
 #
 # Note that you can always access the default settings by
 # >>> from dynlib import default_conf
