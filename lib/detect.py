626d88b387b2f8c47824d85b7f0b0a1a16374cef
Clemens Spensberger <csp001@uib.no>
2015-03-23 16:45:04 +0100

    Added python modules for the remaining fortran modules, and created a shorthands file to make the abbreviations available that used to be part of __init__.py

--- lib/detect.py
+++ lib/detect.py
@@ -4,7 +4,6 @@ import os
 import sys
 import pickle
 
-import numpy as np
 import dynfor
 
 
@@ -18,42 +17,13 @@ else:
 	print 'Warning: Fortran documentation not found. Make sure you have compiled dynlib.'
 	fortran_doc = {}
 
-for name, value in dynfor.stat.__dict__.items():
+for name, value in dynfor.detect.__dict__.items():
 	# Backup the f2py generated docstring
 	value.__f2pydoc__ = value.__doc__
 	# Set the doctring from the fortran_doc extracted documentation
 	value.__doc__ = fortran_doc.get('utils.%s' % name, None)
 	# Make available as a member of this python module
 	locals()[name] = value
-
-
-
-
-def running_mean(ts, rlen=8):
-	rmean = np.zeros((len(ts)-rlen+1,))
-	for i in range(len(ts)-rlen+1):
-		rmean[i] = sum(ts[i:i+rlen])/rlen
-	
-	return rmean
-
-
-def running_mean_periodic(ts, rlen=3):
-	rmean = np.zeros(len(ts))
-	for i in range(len(ts)):
-		for j in np.arange(rlen)-rlen/2:
-			rmean[i] += ts[(i+j)%len(ts)]
-	
-	rmean /= rlen
-
-	return rmean
-
-
-def filter7p(ts):
-	rmean = np.zeros((len(ts)-6))
-	for i in range(len(ts)-6):
-		rmean[i] = (ts[i]-6*ts[i+1]+15*ts[i+2]+44*ts[i+3]+15*ts[i+4]-6*ts[i+5]+ts[i+6])/64.0
-	
-	return rmean
 
 
 #
---
+++ lib/shorthands.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8
+
+# Importing some widely used functions in a common name space
+
+import dynfor
+
+import figures as fig
+import gridlib as gridlib
+import utils as utils
+import settings as settings
+from metopen import metopen, metsave, metsave_lines, get_instantaneous, get_static
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+from datetime import datetime as dt, timedelta as td
+
+
+# Saving the default configuration
+from copy import deepcopy
+default_conf = deepcopy(settings.conf)
+del deepcopy
+
+
+# that's it
