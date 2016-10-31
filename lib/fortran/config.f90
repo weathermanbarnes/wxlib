42d3c9d1dd2a43d5c69d43d43381267ed4ff65f4
Clemens Spensberger <csp001@uib.no>
2016-10-31 03:17:13 +0100

    Tested frontline detection and adapted default threshold
    Threshold set to a value vaguely appropriate for considerably smoothed
    ERA-Interim data (n=30 for smoothing).

--- lib/fortran/config.f90
+++ lib/fortran/config.f90
@@ -9,7 +9,7 @@ module config
   logical :: grid_cyclic_ew = .false.
   !
   ! Front / Line / Axis detection settings
-  real(kind=nr) :: frint_thres =-11.7e-11_nr, &
+  real(kind=nr) :: frint_thres =-2.0e-11_nr, &
     &              frspd_thres = 1.5_nr, &
     &                vor_thres = 0.7e-4_nr, &
     &                div_thres =-0.25e-4_nr, &
